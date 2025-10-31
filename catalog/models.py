fb04344419460391bc32b0fe1b8945ff0f942d18
kostya261 <kos26193@gmail.com>
2025-10-31 21:40:32 +0300

    домашнее задание 23 добавил миграции

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.template.defaultfilters import title
+
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='photos/', verbose_name='Фотография')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='категория')
+    price = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField()
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['title',]
+
--- config/settings.py
+++ config/settings.py
@@ -9,10 +9,15 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
+from django.conf.global_settings import MEDIA_URL, MEDIA_ROOT
+from dotenv import load_dotenv
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS
+load_dotenv(override=True)
+
+#from django.conf.global_settings import STATICFILES_DIRS, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nlq^q8j(y&$&l_*0aw()c7@fr=%*u73p7r&g^+ho63g(4cn8rc'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -74,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
@@ -113,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
