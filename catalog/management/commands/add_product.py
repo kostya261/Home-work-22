1edb27b39faa04e3e1d5056da5fd8a23065e03c3
kostya261 <kos26193@gmail.com>
2025-11-02 01:17:36 +0300

    изменил add_product на загрузку данных из фикстуры

--- catalog/management/commands/add_product.py
+++ catalog/management/commands/add_product.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from catalog.models import Category, Product
 
 
@@ -10,36 +11,42 @@ class Command(BaseCommand):
         Product.objects.all().delete()
         Category.objects.all().delete()
 
-        # Создаем категории (БЕЗ распаковки!)
-        processors = Category.objects.create(title='Процессоры', description='Процессор различных фирм')
-        monitors = Category.objects.create(title='Мониторы', description='Мониторы различных фирм')
-        memorys = Category.objects.create(title='Память', description='Память различных фирм')  # Исправил название!
+       # Заменил на загрузку данных из фикстуры
+        call_command('loaddata', 'catalog_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
 
-        products = [
-            {'title': 'Intel Core i7',
-             'description': 'Процессор фирмы Intel, 4 ядра, 8 потоков, частота процессора 3ГГц',
-             'category': processors, 'price': 25000},  # Добавил цену
-            {'title': 'AMD Rayzen',
-             'description': 'Процессор фирмы AMD, 4 ядра, 8 потоков, частота процессора 3.2ГГц',
-             'category': processors, 'price': 22000},  # Добавил цену
+        """ 
+               # Создаем категории (БЕЗ распаковки!)
+                processors = Category.objects.create(title='Процессоры', description='Процессор различных фирм')
+                monitors = Category.objects.create(title='Мониторы', description='Мониторы различных фирм')
+                memorys = Category.objects.create(title='Память', description='Память различных фирм')
 
-            {'title': 'Monitor Acer',
-             'description': 'Монитор фирмы Acer', 'price': 5000, 'category': monitors},
-            {'title': 'Monitor Benq',
-             'description': 'Монитор фирмы Benq', 'price': 6000, 'category': monitors},
-            {'title': 'Monitor Samsung',
-             'description': 'Монитор фирмы Samsung', 'price': 9000, 'category': monitors},
+                products = [
+                    {'title': 'Intel Core i7',
+                     'description': 'Процессор фирмы Intel, 4 ядра, 8 потоков, частота процессора 3ГГц',
+                     'category': processors, 'price': 25000},
+                    {'title': 'AMD Rayzen',
+                     'description': 'Процессор фирмы AMD, 4 ядра, 8 потоков, частота процессора 3.2ГГц',
+                     'category': processors, 'price': 22000}, 
 
-            {'title': 'Memory Kingston',
-             'description': 'Память Kingston', 'price': 800, 'category': memorys},
-            {'title': 'Memory Hynix',
-             'description': 'Память Hynix', 'price': 900, 'category': memorys},
-        ]
+                    {'title': 'Monitor Acer',
+                     'description': 'Монитор фирмы Acer', 'price': 5000, 'category': monitors},
+                    {'title': 'Monitor Benq',
+                     'description': 'Монитор фирмы Benq', 'price': 6000, 'category': monitors},
+                    {'title': 'Monitor Samsung',
+                     'description': 'Монитор фирмы Samsung', 'price': 9000, 'category': monitors},
 
-        for product_data in products:  # Переименовал переменную
-            product, created = Product.objects.get_or_create(**product_data)
-            if created:
-                self.stdout.write(
-                    self.style.SUCCESS(f'Successfully added product: {product.title}'))  # Исправил SUCCES на SUCCESS
-            else:
-                self.stdout.write(self.style.WARNING(f'Product: {product.title} already exist!'))
+                    {'title': 'Memory Kingston',
+                     'description': 'Память Kingston', 'price': 800, 'category': memorys},
+                    {'title': 'Memory Hynix',
+                     'description': 'Память Hynix', 'price': 900, 'category': memorys},
+                ]
+
+                for product_data in products:  # Переименовал переменную
+                    product, created = Product.objects.get_or_create(**product_data)
+                    if created:
+                        self.stdout.write(
+                            self.style.SUCCESS(f'Successfully added product: {product.title}'))
+                    else:
+                        self.stdout.write(self.style.WARNING(f'Product: {product.title} already exist!'))
+                """
